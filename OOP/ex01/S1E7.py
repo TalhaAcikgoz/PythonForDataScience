9228765af7f215fefba13258902abc16fb18e4c2
TalhaAcikgoz <tacikgoz58@gmail.com>
2024-06-08 18:26:03 +0300

    OOP init

---
+++ OOP/ex01/S1E7.py
@@ -0,0 +1,12 @@
+from S1E9 import Character
+
+
+class Baratheon(Character):
+    """ A class representing a Baratheon character."""
+    pass
+
+class Lannister(Character):
+    """A class representing a Lannister character."""
+    def create_lannister():
+        """Creates a Lannister character."""
+        pass
---
+++ OOP/ex01/S1E9.py
@@ -0,0 +1,20 @@
+from abc import ABC, abstractmethod
+
+
+class Character(ABC):
+    """Your docstring for character class"""
+    @abstractmethod
+    def __init__(self, first_name: str, is_alive = True):
+        """ Your docstring for Character constructor method"""
+        self.first_name = first_name
+        self.is_alive = is_alive
+
+
+class Stark(Character):
+    """Your docstring for Stark class"""
+    def __init__(self, first_name: str, is_alive= True):
+        """Your docstring for Stark constructor method"""
+        super().__init__(first_name, is_alive)
+    def die(self):
+        """Your docstring for Stark die method"""
+        self.is_alive = False
