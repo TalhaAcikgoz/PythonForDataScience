535cd630b044caa94631a1b41e7b6dc6614324e5
TalhaAcikgoz <tacikgoz58@gmail.com>
2024-06-09 16:52:18 +0300

    OOP update

--- OOP/ex01/S1E7.py
+++ OOP/ex01/S1E7.py
@@ -3,10 +3,26 @@ from S1E9 import Character
 
 class Baratheon(Character):
     """ A class representing a Baratheon character."""
-    pass
+    def __init__(self, first_name: str, is_alive=True, eyes="brown", hairs="dark"):
+        """Initializes a Lannister character."""
+        super().__init__(first_name, is_alive)
+        self.eyes = eyes
+        self.hairs = hairs
+        self.family_name = "Baratheon"
+    
+    def __str__(self) -> str:
+        return f"<bound method Baratheon.__str__ of Vector: ({self.family_name}, {self.eyes}, {self.hairs})>"
+
 
 class Lannister(Character):
     """A class representing a Lannister character."""
-    def create_lannister():
+    def __init__(self, first_name: str, is_alive=True, eyes="blue", hairs="light"):
+        """Initializes a Lannister character."""
+        super().__init__(first_name, is_alive)
+        self.eyes = eyes
+        self.hairs = hairs
+        self.family_name = "Lannister"
+    
+    def create_lannister(first_name: str, is_alive=True, eyes="blue", hairs="light"):
         """Creates a Lannister character."""
-        pass
+        return Lannister(first_name, is_alive, eyes, hairs)
